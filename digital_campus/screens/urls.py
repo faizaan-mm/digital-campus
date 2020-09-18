4d2920c344ba0066acd09ca24f17feff5d0b127c
adis98 <aditya.ssr@gmail.com>
2020-09-18 08:36:24 +0530

    added new pages. Models pending

--- digital_campus/screens/urls.py
+++ digital_campus/screens/urls.py
@@ -1,5 +1,11 @@
 from django.urls import path
+from django.urls import re_path
 
 from . import views
 
-urlpatterns = [path('',views.login,name= 'login'),]
+urlpatterns = [re_path(r'^Login',views.Login,name= 'Login'),
+               re_path(r'^chatbox',views.chat,name='chat'),
+               re_path(r'^homep',views.homep,name='homep'),
+               re_path(r'^Homepage',views.Homepage,name='Homepage'),
+               re_path(r'^prsnl',views.prsnl,name='prsnl'),
+               re_path(r'^Schedule',views.Schedule_class,name='Schedule_class'),]
--- digital_campus/screens/views.py
+++ digital_campus/screens/views.py
@@ -2,7 +2,32 @@ from django.shortcuts import render
 from django.http import HttpResponse, Http404
 from django.template import loader
 
-def login(request):
-    template = loader.get_template('screens/index.html')
+def Login(request):
+    template = loader.get_template('screens/Login.html')
+    context = {}
+    return HttpResponse(template.render(context, request))
+
+def chat(request):
+    template = loader.get_template('screens/chatbox intro.html')
+    context = {}
+    return HttpResponse(template.render(context, request))
+
+def homep(request):
+    template = loader.get_template('screens/homep_enroll.html')
+    context = {}
+    return HttpResponse(template.render(context, request))
+
+def Homepage(request):
+    template = loader.get_template('screens/Homepage.html')
+    context = {}
+    return HttpResponse(template.render(context, request))
+    
+def prsnl(request):
+    template = loader.get_template('screens/prsnl_dtls.html')
+    context = {}
+    return HttpResponse(template.render(context, request))
+
+def Schedule_class(request):
+    template = loader.get_template('screens/Schedule class.html')
     context = {}
     return HttpResponse(template.render(context, request))
