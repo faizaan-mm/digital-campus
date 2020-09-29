9f0560efac7c971ca2fd4dec95ae589b93bada09
Nilufer2703 <56989591+nilufer2703@users.noreply.github.com>
2020-09-29 20:33:31 +0530

    Add files via upload

--- digital_campus/onlineclass/views.py
+++ digital_campus/onlineclass/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
-def index(request):
-    '''
-    Index(Home Page) View
-    '''
-    return render(request, 'onlineclass/index.html')
+def login(request):
+    return render(request,'index.html')
